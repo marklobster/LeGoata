8b435d0cb6fd6a516e31c7788fb8e4822390668c
Mark L <marklobster@gmail.com>
2025-06-16 12:32:25 -0400

    sample initializer so Main doesn't crash

--- src/org/legoata/Main.java
+++ src/org/legoata/Main.java
@@ -1,16 +1,28 @@
 package org.legoata;
 
+import org.legoata.event.GameCycleEvent;
+import org.legoata.event.GameCycleEventHandler;
+import org.legoata.execute.ControlSet;
 import org.legoata.execute.GameRunner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		try {
-			System.out.println("Hello Goats!  Let's get battlin'!");
+			System.out.println("Hello Goats!  Here's a sample game initializer!");
 			System.out.println();
 			
 			// configure game
 			GameRunner game = new GameRunner();
+			game.setInitializer(new GameCycleEventHandler() {
+
+				@Override
+				public void consume(GameCycleEvent event, ControlSet controls) {
+					controls.getGameControls().getOutStream().print("Hello! Time to exit!");
+					controls.getGameControls().setExitFlag(true);
+				}
+				
+			});
 			
 			// start game
 			game.run();
