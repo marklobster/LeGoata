2233ed1c866055b405154cb5390082fbab73471b
Mark L <marklobster@gmail.com>
2025-06-24 08:54:59 -0400

    Book, utils

---
+++ src/org/legoata/samples/gofish/util/GoFishUtils.java
@@ -0,0 +1,52 @@
+package org.legoata.samples.gofish.util;
+
+import org.legoata.samples.gofish.model.Rank;
+import org.legoata.samples.gofish.model.Suit;
+
+public class GoFishUtils {
+	public static String getString(Rank rank) {
+		switch (rank) {
+		case KING:
+			return "King";
+		case QUEEN:
+			return "Queen";
+		case JACK:
+			return "Jack";
+		case TEN:
+			return "Ten";
+		case NINE:
+			return "Nine";
+		case EIGHT:
+			return "Eight";
+		case SEVEN:
+			return "Seven";
+		case SIX:
+			return "Six";
+		case FIVE:
+			return "Five";
+		case FOUR:
+			return "Four";
+		case THREE:
+			return "Three";
+		case TWO:
+			return "Two";
+		default:
+			return "Ace";
+		}
+	}
+	public static String getPluralString(Rank rank) {
+		return getString(rank) + "s";
+	}
+	public static String getString(Suit suit) {
+		switch (suit) {
+		case CLUB:
+			return "Clubs";
+		case DIAMOND:
+			return "Diamonds";
+		case HEART:
+			return "Hearts";
+		default:
+			return "Spades";
+		}
+	}
+}
