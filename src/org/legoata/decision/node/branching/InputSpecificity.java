65ea1d401f1a0e130d21f8e985b1c26eb6df1fb5
Mark L <marklobster@gmail.com>
2025-12-05 11:14:37 -0500

    code doc

--- src/org/legoata/decision/node/branching/InputSpecificity.java
+++ src/org/legoata/decision/node/branching/InputSpecificity.java
@@ -1,7 +1,19 @@
 package org.legoata.decision.node.branching;
 
+/**
+ * The level of specificity required when checking an input against keys. This is utilized by the Menu in this same package.
+ */
 public enum InputSpecificity {
+	/**
+	 * User input must be exact match to key
+	 */
 	EXACT_MATCH,
+	/**
+	 * User input must match all characters in key, but case is ignored
+	 */
 	CASE_INSENSITIVE,
+	/**
+	 * User input is matched to the key that contains most matching characters, ignoring case, and starting at index 0
+	 */
 	CLOSEST_MATCH
 }
--- src/org/legoata/decision/node/branching/ListDisplayMode.java
+++ src/org/legoata/decision/node/branching/ListDisplayMode.java
@@ -1,11 +1,23 @@
 package org.legoata.decision.node.branching;
 
 /**
- * 
+ * Display modes for the Menu's option list.
  */
 public enum ListDisplayMode {
+	/**
+	 * Display Option keys and titles, separated by the separator
+	 */
 	KEYS_AND_TITLES,
+	/**
+	 * Display Option keys only
+	 */
 	KEYS_ONLY,
+	/**
+	 * Have Menu set the option keys by numbering them, starting at 0; key and title separated by separator
+	 */
 	NUMBER_FROM_ZERO,
+	/**
+	 * Have Menu set the option keys by numbering them, starting at 1; key and title separated by separator
+	 */
 	NUMBER_FROM_ONE
 }
